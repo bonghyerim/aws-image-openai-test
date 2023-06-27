0bb54dbb9cef0ea87648a2ee3c250fbafcfcd8e1
bonghyerim <130967304+bonghyerim@users.noreply.github.com>
2023-06-27 10:13:59 +0900

    add

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 
 from resources.photo import PhotoResource, PhotoReviewResource
 from resources.search import NewsSearchResource
+from resources.translate import TranslateResource
 
 app = Flask(__name__)
 
@@ -11,9 +12,7 @@ api = Api(app)
 api.add_resource( PhotoResource ,  '/photo')
 api.add_resource( PhotoReviewResource  , '/review')
 api.add_resource( NewsSearchResource  , '/search/news')
+api.add_resource( TranslateResource  , '/translate')
 
 if __name__ == '__main__':
     app.run()
-
-
-
---
+++ resources/translate.py
@@ -0,0 +1,40 @@
+
+from flask_restful import Resource
+from flask import request
+
+## restful api 를 코드에서 사용할때 필요한 라이브러리 ##
+import requests
+
+from config import Config
+
+class TranslateResource(Resource):
+
+    def post(self):
+        
+        data = request.get_json()
+
+        headers = { 'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8',
+                    'X-Naver-Client-Id': Config.X_NAVER_CLIENT_ID,
+                    'X-Naver-Client-Secret':Config.X_NAVER_CLIENT_SECRET}
+
+
+        data = {'source' : 'ko',
+                'target':data['lang'],
+                'text':data['text']}
+
+        response = requests.post('https://openapi.naver.com/v1/papago/n2mt',
+                      headers=headers,
+                      data=data)
+        
+
+        print(response.json())
+        
+        print()
+
+        print(response.json()['message']['result']['translatedText'])
+        
+        
+        text = response.json()['message']['result']['translatedText']
+
+
+        return { 'result' : 'success', 'text' : text }
